648fc8d0faaf7cf850b40a328de80ead946719cd
Ly-TE <528179877@qq.com>
2023-10-12 22:52:17 +0800

    20231012发送get请求

--- testcase/send_request.py
+++ testcase/send_request.py
@@ -1,20 +1,31 @@
 import requests
 
-rep = requests.request()
+# rep = requests.request()
 
-# 返回字符串数据
-print(rep.text)
-# 返回字节数据
-print(rep.content)
-# 返回字典数据
+# # 返回字符串数据
+# print(rep.text)
+# # 返回字节数据
+# print(rep.content)
+# # 返回字典数据
+# print(rep.json())
+# # 状态码
+# print(rep.status_code)
+# # 返回状态信息
+# print(rep.reason)
+# # 返回cookie信息
+# print(rep.cookies)
+# # 返回编码格式
+# print(rep.encoding)
+# # 返回响应头信息
+# print(rep.headers)
+
+# 发送get请求
+url = "https://api.weixin.qq.com/cgi-bin/token"
+data = {
+    "grant_type": "client_credential",
+    "appid": "wx51f605c204d32479",
+    "secret": "4d90172f3c8d290c44409d1999893ccf"
+}
+rep = requests.get(url=url, params=data)
 print(rep.json())
-# 状态码
-print(rep.status_code)
-# 返回状态信息
-print(rep.reason)
-# 返回cookie信息
-print(rep.cookies)
-# 返回编码格式
-print(rep.encoding)
-# 返回响应头信息
-print(rep.headers)
+
