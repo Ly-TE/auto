f162dff8d0b5d428cd8cb0b6fd44865baec1c904
Ly-TE <528179877@qq.com>
2023-10-14 17:25:02 +0800

    feat:代码结构优化

--- run/test_case.py
+++ run/test_case.py
@@ -1,64 +1,74 @@
-from utils import *
-import time
-import json
 import requests
 
-class TestBili:
-    account_token = ''
 
-    def setup_class(self):
-        self.yaml_data = YamlUtil('tese_case.yml').read_yaml
+class TestApi:
+    # 类变量
+    access_token = ""
 
-    def setup_method(self, method):
-        data = self.yaml_data[method.__name__]
-        self.url, self.method, self.params = data['url'], data['method'], data['params']
+    # 获取access_token鉴权码接口
+    def test_get_token(self):
+        urls = "https://api.weixin.qq.com/cgi-bin/token"  # 获取access_token的API地址
+        datas = {
+            "grant_type": "client_credential",  # 授权类型为客户端凭证
+            "appid": "wx51f605c204d32479",  # 应用ID
+            "secret": "4d90172f3c8d290c44409d1999893ccf"  # 应用密钥
+        }
+        try:
+            res = requests.post(url=urls, params=datas)  # 发送POST请求
+            print(res.json())  # 打印返回的json数据
+            res.raise_for_status()  # 如果返回状态码不是200，抛出异常
+            TestApi.access_token = res.json()["access_token"]  # 从返回的json数据中获取access_token，保存到类变量中
+            print(TestApi.access_token)
+        except requests.exceptions.RequestException as e:
+            print("发生异常:", e)
 
-    def test_search(self):
-        print(self.yaml_data)
-        data = self.yaml_data['test_search']
-        url, method, params = data['url'], data['method'], data['params']
+    # 查询标签接口
+    def test_select_flag(self):
+        urls = "https://api.weixin.qq.com/cgi-bin/tags/get"  # 查询标签的API地址
+        datas = {
+            "access_token": TestApi.access_token  # 通过类变量获取access_token
+        }
+        try:
+            res = requests.get(url=urls, params=datas)  # 发送GET请求
+            res.raise_for_status()  # 如果返回状态码不是200，抛出异常
+            print(res.json())  # 打印返回的json数据
+        except requests.exceptions.RequestException as e:
+            print("发生异常:", e)
 
-        # rep = req.call(url, method, params=params).json()
-        # assert rep['data']['page']['count'] == 29
-        # self.params['wts'] =  str(int(time.time()))
-        # print(self.url, self.method, self.params,'ccccc')呜呜呜呜v
-        # headers = {
-        #     'Accept': 'application/json, text/plain, */*',
-        #     'Accept-Encoding': 'gzip, deflate',
-        #     'Accept-Language': 'zh-CN,zh;q=0.9',
-        #     'Cookie': "buvid3=EE608266-D29E-3D39-02E6-DA137654450128347infoc; b_nut=1671604728; _uuid=8105D6454-33DE-D8"
-        #               "98-2D4F-AA51CC7F9C8429050infoc; CURRENT_FNVAL=4048; rpdid=|(u)~J~R~RlJ0J'uY~k~JuRR~; i-wanna-go-"
-        #               "back=-1; buvid4=92A2A25E-2E49-2404-9163-631F457C2E7D29593-022122114-AOc2%2FD4Q7%2B%2Bb6wPB7dInRA"
-        #               "%3D%3D; SESSDATA=9818423f%2C1692011031%2C670f4%2A22; bili_jct=9cc0b5211dd3c9e6e491a3b9eedfc214; "
-        #               "DedeUserID=443106609; DedeUserID__ckMd5=f083fe41094c7ad2; sid=7yunjkqc; b_ut=5; header_theme_ver"
-        #               "sion=CLOSE; nostalgia_conf=-1; home_feed_column=5; CURRENT_PID=1675d090-cab3-11ed-9e53-e7331738a"
-        #               "adb; buvid_fp_plain=undefined; LIVE_BUVID=AUTO7016804854491132; CURRENT_QUALITY=80; hit-new-styl"
-        #               "e-dyn=0; hit-dyn-v2=1; PVID=1; FEED_LIVE_VERSION=V8; fingerprint=6a9d3d875674fffb7ccd38b64c64607"
-        #               "0; buvid_fp=4f35bb21a1ed92129737cd425d2c1eeb; bp_video_offset_443106609=806265327090401300; b_ls"
-        #               "id=65E8610E6_188AF598BCF; innersign=0; bsource=search_baidu; browser_resolution=2560-1329",
-        #     'Origin': 'https://space.bilibili.com',
-        #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.'
-        #                   '0.0.0 Safari/537.36'
-        # }
-        # rep = req.call(self.url, self.method, params=self.params).json()
-        # rep = requests.get(url='https://api.bilibili.com/x/space/wbi/arc/search?mid=170901672&ps=30&tid=0&pn=1&keyword='
-        #                        '&order=pubdate&platform=web&web_location=1550101&order_avoided=true&w_rid=e5b3ff6c4957d'
-        #                        'c2b1e9c76516ba87589&wts=1686569270',headers=headers)
-        # print('reeeeeeee',rep)
-        # assert rep['data']['page']['count'] == 29
-    #
-    # def test_getSettings(self):
-    #     # data = self.yaml_data['test_getSettings']
-    #     # url, method, params = data['url'], data['method'], data['params']
-    #     # rep = req.call(url, method, params=params).json()
-    #     # assert rep['status'] == True
-    #     rep = req.call(self.url, self.method, params=self.params).json()
-    #     assert rep['status'] == True
-    #
-    # def test_locs(self):
-    #     rep = req.call(self.url, self.method, params=self.params).json()
-    #     assert rep['count'] == 1
+    # 编辑标签接口
+    def test_edit_flag(self):
+        urls = "https://api.weixin.qq.com/cgi-bin/tags/update?access_token=" + TestApi.access_token  # 编辑标签的API地址，通过类变量获取access_token
+        payload = {
+            'id': 120,  # 标签ID
+            'name': '广东人111'  # 修改后的标签名称
+        }
+        try:
+            res = requests.post(url=urls, json=payload)  # 发送POST请求，并将修改标签的数据作为Json数据发送到API
+            res.raise_for_status()  # 如果返回状态码不是200，抛出异常
+            print(res.json())  # 打印返回的json数据
+        except requests.exceptions.RequestException as e:
+            print("发生异常:", e)
 
-    def test_geoip(self):
-        res = req.call(self.url, self.method, params=self.params).json()
-        assert res['data']['province_code'] == 'HB'
+    # 文件上传接口
+    def test_files_upload(self):
+        urls = "https://api.weixin.qq.com/cgi-bin/media/uploadimg"  # 文件上传的API地址
+        datas = {
+            "access_token": TestApi.access_token  # 通过类变量获取access_token
+        }
+        files = {
+            "media": open("E:/shu.jpg", "rb")  # 要上传的文件路径，这里假设为 E:/shu.jpg
+        }
+        try:
+            res = requests.post(url=urls, files=files, params=datas)  # 发送POST请求，并将文件作为二进制流发送到API
+            res.raise_for_status()  # 如果返回状态码不是200，抛出异常
+            print(res.json())  # 打印返回的json数据
+        except requests.exceptions.RequestException as e:
+            print("发生异常:", e)
+
+
+if __name__ == '__main__':
+    test_api = TestApi()  # 创建TestApi类的实例
+    test_api.test_get_token()  # 调用获取access_token的方法
+    test_api.test_select_flag()  # 调用查询标签的方法
+    test_api.test_edit_flag()  # 调用编辑标签的方法
+    test_api.test_files_upload()  # 调用文件上传的方法
--- utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from utils.yaml_utils import YamlUtil
-from utils.request_utils import req
